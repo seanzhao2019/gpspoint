9102bc709f972bc61cb6fe9146d81a3deb4e7908
zhao <codzhao@outlook.com>
2015-07-26 00:21:50 +0800

    20150725

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # gpspoint
 20150722:需要之后将userdat的view.py进行优化，包括更新数据库entry   
-20150723:增加了对多输入的支持
+20150723:增加了对多输入的支持    
+20150725:对Userdata数据库和Sensordata数据库进行了修改，userdata的输入变成单输入，并且返回pk值。加入对sensordata的收集
--- userdata/models.py
+++ userdata/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Userdata(models.Model):
-    user_id=models.IntegerField(primary_key=True)
+    user_id=models.IntegerField()
     username = models.CharField(max_length=30)
     gender = models.CharField(max_length=10)    
     age = models.IntegerField()
@@ -10,7 +10,19 @@ class Userdata(models.Model):
     emergency_number = models.IntegerField()
     destination=models.CharField(max_length=20)
     
+    class Meta:
+        unique_together = (("user_id", "username"),)
 
     def __unicode__(self):
         return self.username
+
+class Sensordata(models.Model):
+    user=models.ForeignKey('Userdata')#Userdata's Primary Key
+    timestamp=models.DateTimeField()
+    lan=models.FloatField(null=True)
+    lon=models.FloatField(null=True)
+    
+
+#    def __unicode__(self):
+#        return self.timestamp
 
